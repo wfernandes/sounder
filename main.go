d8b33081c8d7b93ef0a1665f53b7d74864e24d3f
Alex Jackson <ajackson@pivotallabs.com>
2014-10-04 08:52:45 -0600

    move to noaa lib

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/cloudfoundry/dropsonde/autowire/metrics"
 	"github.com/cloudfoundry/dropsonde/autowire/logs"
-	"github.com/cloudfoundry/loggregator_consumer/dropsonde_consumer"
+	"github.com/cloudfoundry/dropsonde/events"
+	"github.com/cloudfoundry/noaa"
 	"fmt"
-	"time"
 	"crypto/tls"
+	"encoding/json"
 	"os"
+	"io/ioutil"
+	"path/filepath"
 )
+
+type config struct {
+	AccessToken string
+}
 
 func main() {
 	app := cli.NewApp()
@@ -43,37 +50,91 @@ func main() {
 		{
 			Name:      "stream",
 			ShortName: "s",
-			Usage:     "stream dropsonde messages",
+			Usage:     "stream messages",
 			Action: func(c *cli.Context) {
-				consumer := dropsonde_consumer.NewDropsondeConsumer(c.Args().First(), &tls.Config{InsecureSkipVerify: true}, nil)
-				messages, err := consumer.Stream(c.Args().Get(1), c.Args().Get(2))
+				consumer := noaa.NewNoaa(c.Args().First(), &tls.Config{InsecureSkipVerify: true}, nil)
+				messages, err := consumer.Stream(c.Args().Get(1), authToken())
 				if err != nil {
 					panic(err)
 				}
 				for message := range messages {
-					ts := message.GetTimestamp()
-					timestamp := time.Unix(0, ts)
-					fmt.Printf("%s - Type: %s\n", timestamp.String(), message.GetEventType().String())
+					displayMessage(message)
 				}
 			},
 		},
-
 		{
 			Name:      "recent",
 			ShortName: "r",
-			Usage:     "recent dropsonde log messages",
+			Usage:     "recent log messages",
 			Action: func(c *cli.Context) {
-				consumer := dropsonde_consumer.NewDropsondeConsumer(c.Args().First(), &tls.Config{InsecureSkipVerify: true}, nil)
-				messages, err := consumer.RecentLogs(c.Args().Get(1), c.Args().Get(2))
+				consumer := noaa.NewNoaa(c.Args().First(), &tls.Config{InsecureSkipVerify: true}, nil)
+				messages, err := consumer.RecentLogs(c.Args().Get(1), authToken())
 				if err != nil {
 					panic(err)
 				}
 				for _, message := range messages {
-					timestamp := time.Unix(0, message.GetTimestamp())
-					fmt.Printf("%s - Type: %s\n", timestamp.String(), message.GetEventType().String())
+					displayMessage(message)
+				}
+			},
+		},
+		{
+			Name:      "tail logs",
+			ShortName: "t",
+			Usage:     "tail log messages",
+			Action: func(c *cli.Context) {
+				consumer := noaa.NewNoaa(c.Args().First(), &tls.Config{InsecureSkipVerify: true}, nil)
+				messages, err := consumer.TailingLogs(c.Args().Get(1), authToken())
+				if err != nil {
+					panic(err)
+				}
+				for message := range messages {
+					displayMessage(message)
+				}
+			},
+		},
+		{
+			Name:      "firehose",
+			ShortName: "f",
+			Usage:     "all data",
+			Action: func(c *cli.Context) {
+				consumer := noaa.NewNoaa(c.Args().First(), &tls.Config{InsecureSkipVerify: true}, nil)
+				messages, err := consumer.Firehose(authToken())
+				if err != nil {
+					panic(err)
+				}
+				for message := range messages {
+					displayMessage(message)
 				}
 			},
 		},
 	}
 	app.Run(os.Args)
+}
+
+func displayMessage(m *events.Envelope) {
+	fmt.Printf("%v \n", m)
+}
+
+func authToken() string {
+	var c config
+
+	configDir := filepath.Join(os.Getenv("HOME"), ".cf")
+	file, err := os.Open(filepath.Join(configDir, "config.json"))
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(bytes, &c)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("TOKEN:", c.AccessToken)
+
+	return c.AccessToken
 }
